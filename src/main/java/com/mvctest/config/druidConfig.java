03e157469d59bd9fbc318941f8b45f5eb48882c8
Steven Ramsey <ztcmiku@gmail.com>
2023-05-20 00:26:16 +0800

    add dataSource

--- src/main/java/com/mvctest/config/druidConfig.java
+++ src/main/java/com/mvctest/config/druidConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 @Configuration
 public class druidConfig {
+	//add dataSource
     @Bean
     public DataSource dataSource(){
         DruidDataSource dataSource=new DruidDataSource();
